e4c79fe48f8b2ac4ec60001775a346521df846de
awiles808 <awiles808@gmail.com>
2018-08-19 21:36:11 -0600

    updated

--- vid-1-simple-example/start/app.js
+++ vid-1-simple-example/start/app.js
@@ -2,4 +2,14 @@ const myList = document.getElementsByTagName('li');
 
 for (let i = 0; i < myList.length; i ++) {
   myList[i].style.color = 'purple';
-};
+}
+
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
+  for (let i = 0; i < errorNotPurple.length; i ++) {
+    errorNotPurple[i].style.color = 'red';
+  }
+
+  const odds= document.querySelectorAll('li:nth-child(even)');
+    for (let i = 0; i < odds.length; i ++) {
+      odds[i].style.backgroundColor = 'lightgray';
+    }
