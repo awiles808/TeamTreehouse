e431c276f1a338426ade3e26ec2db206ad00fe84
awiles808 <awiles808@gmail.com>
2018-08-27 23:19:20 -0600

    Importance of picking good names for variables. Created elements to the DOM

--- TheDOM/vid-2-changing-element-attributes/start/app.js
+++ TheDOM/vid-2-changing-element-attributes/start/app.js
@@ -1,8 +1,27 @@
-const input = document.querySelector('input');
-const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+var toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('button.addItemButton');
 
-button.addEventListener('click', () => {
-  p.innerHTML = input.value + ':';
+
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide List'
+      listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show List';
+  listDiv.style.display = 'none';
+}
 });
 
+descriptionButton.addEventListener('click', () => {
+  descriptionp.innerHTML = descriptionInput.value + ':';
+});
+
+addItemButton.addEventListener('click', () => {
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+});
