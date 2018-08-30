6302380d6a7a648b8730e0461edb086272a2e23b
awiles808 <awiles808@gmail.com>
2018-08-29 18:32:30 -0600

    Traversing through the dom and jquery starter files

--- TheDOM/5-traversing-the-DOM/vid-1-using-parent-node/start/app.js
+++ TheDOM/5-traversing-the-DOM/vid-1-using-parent-node/start/app.js
@@ -3,33 +3,79 @@ const listDiv = document.querySelector('.list');
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
+const listUl = document.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
-const removeItemButton = document.querySelector('button.removeItemButton');
+const lis = listUl.children;
+const firstListItem = listUl.firstElementChild;
+const lastListItem = listUl.lastElementChild;
 
-listDiv.addEventListener('mouseover', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toUpperCase();
+
+firstListItem.style.backgroundColor = 'lightskyblue';
+lastListItem.style.backgroundColor = 'salmon';
+
+function attachListItemButtons(li) {
+  let up = document.createElement('button');
+  up.className = 'up';
+  up.textContent = 'Up';
+  li.appendChild(up);
+
+  let down = document.createElement('button');
+  down.className = 'down';
+  down.textContent = 'Down';
+  li.appendChild(down);
+
+  let remove = document.createElement('button');
+  remove.className = 'remove';
+  remove.textContent = 'Remove';
+  li.appendChild(remove);
+
+}
+
+for (let i = 0; i < lis.length; i += 1) {
+  attachListItemButtons(lis[i])
+};
+
+
+listUl.addEventListener('click', (event) => {
+  if (event.target.tagName == 'BUTTON') {
+    if(event.target.className == 'remove') {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    }
+    if(event.target.className == 'up') {
+      let li = event.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      ul = li.parentNode;
+      if(prevLi) {
+      ul.insertBefore(li, prevLi);
+      }
+    }
+    if(event.target.className == 'down') {
+      let li = event.target.parentNode;
+      let nextLi = li.nextElementSibling;
+      ul = li.parentNode;
+      if(nextLi) {
+      ul.insertBefore(nextLi, li);
+      }
+    }
   }
 });
-listDiv.addEventListener('mouseout', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toLowerCase();
-  }
-});
+
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
     toggleList.textContent = 'Hide list';
     listDiv.style.display = 'block';
   } else {
-    toggleList.textContent = 'Show list';                        
+    toggleList.textContent = 'Show list';
     listDiv.style.display = 'none';
-  }                         
+  }
 });
 
 descriptionButton.addEventListener('click', () => {
-  descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionP.innerHTML = descriptionInput.value;
   descriptionInput.value = '';
 });
 
@@ -37,16 +83,7 @@ addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  attachListItemButtons(li);
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
-removeItemButton.addEventListener('click', () => {
-  let ul = document.getElementsByTagName('ul')[0];
-  let li = document.querySelector('li:last-child');
-  ul.removeChild(li);
-});
-  
-  
-  
-  
