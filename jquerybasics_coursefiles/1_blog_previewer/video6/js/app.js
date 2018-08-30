1f9882c423e45d6d381eb384c35cca62aa550534
awiles808 <awiles808@gmail.com>
2018-08-30 09:22:24 -0600

    Blog Previewer

--- jquerybasics_coursefiles/1_blog_previewer/video6/js/app.js
+++ jquerybasics_coursefiles/1_blog_previewer/video6/js/app.js
@@ -1,11 +1,20 @@
-$('#flashMessage')
-  .hide()
-  .slideDown(1000)
-  .delay(3000)
-  .slideUp();
+// $('#flashMessage')
+//   .hide()
+//   .slideDown(1000)
+//   .delay(3000)
+//   .slideUp();
+//
+// const title = "My First Blog Post";
+// const content = "This is my <strong>first</strong> post!";
+//
 
-const title = "My First Blog Post";
-const content = "This is my <strong>first</strong> post!";
 
-$("#blogTitlePreview").text(title);
-$("#blogContentPreview").html(content);
+$('#previewButton').click(function(){
+  const title = $('#blogTitleInput').val();
+  console.log(title);
+  
+//   const content = $('#blogContentInput').val();
+//
+//   $("#blogTitlePreview").text(title);
+//   $("#blogContentPreview").html(content);
+});
