29f6a414ccceff1126741422aec7e8cd43d8780b
awiles808 <awiles808@gmail.com>
2018-08-30 20:52:03 -0600

    Event handling delegation and event propagation in jquery

--- jquerybasics_coursefiles/2_spoiler/video1/js/app.js
+++ jquerybasics_coursefiles/2_spoiler/video1/js/app.js
@@ -1,5 +1,17 @@
 
+$('.spoiler').on('click', 'button', function (e){
+  console.log(e.target);
+     // Show the spoiler text
+  // $('.spoiler span').show();
+    // Hide the "Reveal Spoiler" button
+  $(e.target).hide();
+}) ;
 
 
+//Create the "Reveal Spoiler" previewButton
+const $button = $('<button>Reveal Spoiler</button>');
+//Append to the web page
+$('.spoiler').append($button);
 
-
+//Hide the spoiler text
+$('.spoiler span').hide();
