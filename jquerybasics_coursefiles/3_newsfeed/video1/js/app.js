80805b8e2e72c61d862987b9888708409720c495
awiles808 <awiles808@gmail.com>
2018-08-31 22:19:49 -0600

    Newsfeed

--- jquerybasics_coursefiles/3_newsfeed/video1/js/app.js
+++ jquerybasics_coursefiles/3_newsfeed/video1/js/app.js
@@ -7,3 +7,12 @@ $pdfs.attr('download', true);
 
 $secureLinks.addClass('secure');
 $pdfs.addClass('pdf')
+
+$pdfs.on('click', function(e) {
+  e.preventDefault();
+  //When user clicks on pdf check if checkbox is checked
+  //if zero checkboxes are checked
+  //prevent download of document
+  //alert user
+  //else allow the download
+})
